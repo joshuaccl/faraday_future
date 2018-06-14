9999518e08e90266215c6726bad6761906b6dffb
Joshua Lam <jlam890@hawaii.edu>
2018-06-13 19:06:41 -1000

    Finished up to question 5. Also used convert function on user input to make sure input is valid

--- q2-q5.py
+++ q2-q5.py
@@ -40,10 +40,36 @@ def custom_min(x, y):
 	else:
 		return y
 	
+# Question 5
+
+# Reformatted Code
+
+def plus(x, y):
+	return x+y
+
+def minus(x, y):
+	return x-y
+
+def times(x, y):
+	return x*y
+
+def divide(x, y):
+	return x/y
+
+def apply_operation(left_operand, right_operand, operator):
+	operation_switch = {
+			'+': plus,
+			'-': minus,
+			'*': times,
+			'/': divide
+		}
+
+	func = operation_switch[operator]
+	return func(left_operand, right_operand)
 
 # Test for question two
 def test_two():
-	print("Question two test")
+	print('Question two test')
 	test = str(input("Enter a string to convert ('q' to quit): "))
 	while test != 'q':
 		print(convert(test), type(convert(test)))
@@ -51,28 +77,43 @@ def test_two():
 
 # Test for question four
 def test_four():
-	print("Question four test")
+	print('Question four test')
 	while 1:
-		one = input("Enter the first number ('q' to quit): ")
+		one = convert(input("Enter the first number ('q' to quit): "))
 		if one == 'q':
 			break
-		two = input("Enter the second number ('q' to quit): ")
+		two = convert(input("Enter the second number ('q' to quit): "))
 		if two == 'q':
 			break
-		three = input("Enter the third number ('q' to quit): ")
+		three = convert(input("Enter the third number ('q' to quit): "))
 		if three == 'q':
 			break
-		print("The minimum is " + three_min(one, two, three))
+		print('The minimum is ' + str(three_min(one, two, three)))
 
-# Driver for testing question 2 and 4
-action = input("'q' to quit, '2' or '4' to test corresponding question: ")
+# Test for question five
+def test_five():
+	print('Question five test')
+	while 1:
+		one = convert(input("Enter the first number ('q' to quit): "))
+		if one == 'q':
+			break
+		two = input("Enter the operation ('q' to quit): ")
+		if two == 'q':
+			break
+		three = convert(input("Enter the second number ('q' to quit): "))
+		if three == 'q':
+			break
+		print('The answer is ' + str(convert(apply_operation(one, three, two))))
+
+# Driver for testing question 2 and 4 and 5
+action = input("'q' to quit, '2', '4', or '5' to test corresponding question: ")
 while action != 'q':
 	if action == '2':
 		test_two()
 	elif action == '4':
 		test_four()
+	elif action == '5':
+		test_five()
 	else:
-		print("Please enter 'q', '2', or '4' only")
-	action = input("'q' to quit, '2' or '4' to test corresponding question: ")
-
-
+		print("Please enter 'q', '2', '4', or '5' only")
+	action = input("'q' to quit, '2', '4', or '5' to test corresponding question: ")
